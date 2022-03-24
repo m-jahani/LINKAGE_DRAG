9a48aabd1ef252414818dcfeb00ade5ec532464c
Mojtaba Jahani <moja987@gmail.com>
2022-03-24 11:25:47 -0700

    small update for path

--- MERGE_GWAS.sh
+++ MERGE_GWAS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for GWA in ~/LINKADE_DRAG/GWAS/result/*ps; do
+for GWA in *ps; do
     awk '{print FILENAME , $1,"\t",$2,"\t",$4}' $GWA | sed 's/^Annuus.ann_fullsam.tranche90_snps_bi_AN50_beagle_AF99_//g' | sed 's/.ps//g' | sed 's/__/\t/g' >>all_GWAS
 done
