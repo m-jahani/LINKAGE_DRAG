a981e5830b6c7c6c7048f3f8f05461ec38d7ffc4
Mojtaba Jahani <moja987@gmail.com>
2022-03-30 10:43:19 -0700

    small change in comment

--- JOIN_FRQ2RESULT.R
+++ JOIN_FRQ2RESULT.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 
 args = commandArgs(trailingOnly = TRUE)
-INTFRQ <- args[1] #introg.frquency
+INTFRQ <- args[1] #introg.frequency
 RESULT <- args[2] #GWA,GP, or other results
 SAVE <- args[3] #directory and name to save the merged result
 
@@ -14,6 +14,7 @@ fread(INTFRQ,
         header=T) -> RESULT_FILE
   
   full_join(INTFRQ_FILE,RESULT_FILE) %>%
+    
     fwrite(args[3],
            col.names = T,
            quote = F,
