b088cb1faa583fa1ca1c350f6c2c25babc7ed094
Mojtaba Jahani <moja987@gmail.com>
2022-03-30 10:42:03 -0700

    Rscript for joining introgression to GWA and GP result

---
+++ JOIN_FRQ2RESULT.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(dplyr)
+
+
+args = commandArgs(trailingOnly = TRUE)
+INTFRQ <- args[1] #introg.frquency
+RESULT <- args[2] #GWA,GP, or other results
+SAVE <- args[3] #directory and name to save the merged result
+
+fread(INTFRQ,
+      header=T) -> INTFRQ_FILE
+
+  fread(RESULT,
+        header=T) -> RESULT_FILE
+  
+  full_join(INTFRQ_FILE,RESULT_FILE) %>%
+    fwrite(args[3],
+           col.names = T,
+           quote = F,
+           sep = "\t")
+    
