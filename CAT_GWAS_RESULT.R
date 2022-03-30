dcd6cb1d56fad9b86f0ce8cd86c693d9b531085e
Mojtaba Jahani <moja987@gmail.com>
2022-03-29 17:45:51 -0700

    small change in fwrite

--- CAT_GWAS_RESULT.R
+++ CAT_GWAS_RESULT.R
@@ -16,11 +16,13 @@ for (i in 1:nrow(file_list)) {
     full_join(.,RESULT_Beta) -> RESULT_Beta
 }
 
-fwrite(RESULT_Beta,paste0(PATH,RESULT_Beta),
+fwrite(RESULT_Beta,paste0(PATH,"RESULT_Beta"),
        col.names = T,
        sep = "\t",
        quote = F)
+
 rm(RESULT_Beta)
+
 fread(paste0(PATH, as.character(a[i,1]))) %>%
   select(SNP_ID = V1) -> RESULT_P
 for (i in 1:nrow(file_list)) {
@@ -30,8 +32,9 @@ for (i in 1:nrow(file_list)) {
     full_join(.,RESULT_P) -> RESULT_P
 }
 
-fwrite(RESULT_P,paste0(PATH,RESULT_P),
+fwrite(RESULT_P,paste0(PATH,"RESULT_P"),
        col.names = T,
        sep = "\t",
        quote = F)
+
 rm(RESULT_P)
