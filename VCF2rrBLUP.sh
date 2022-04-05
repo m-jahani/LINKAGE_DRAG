b4876aef77a9723b3c8be69a41deebe2e00b20f0
Mojtaba Jahani <moja987@gmail.com>
2022-04-04 19:35:08 -0700

    delete extra files

--- VCF2rrBLUP.sh
+++ VCF2rrBLUP.sh
@@ -8,5 +8,5 @@ while read SAMPLE; do
     vcftools --vcf $VCF --indv $SAMPLE --recode --recode-INFO-all --out $SAMPLE
     java -jar /DATA/home/mjahani/bin/NGSEPcore_4.2.0/NGSEPcore.jar VCFConverter -rrBLUP -i ${SAMPLE}.recode.vcf -o ${VCF%%vcf}_${SAMPLE}
     cat ${VCF%%vcf}_${SAMPLE}_rrBLUP.in >>${VCF%%.vcf}_rrBLUP.in
-    rm ${VCF%%vcf}_${SAMPLE}_rrBLUP.in ${VCF%%vcf}_${SAMPLE}_rrBLUP_samples.txt
+    rm ${VCF%%vcf}_${SAMPLE}_rrBLUP.in ${VCF%%vcf}_${SAMPLE}_rrBLUP_samples.txt ${SAMPLE}.log ${SAMPLE}.recode.vcf
 done <${VCF%%vcf}sample_list
