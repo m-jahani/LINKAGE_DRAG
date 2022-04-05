507c18bfb6a88235a92d44a52e79ae4cff9ead17
Mojtaba Jahani <moja987@gmail.com>
2022-04-04 18:04:55 -0700

    script for converting VCF to rrBLUP format

---
+++ VCF2rrBLUP.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+VCF=$1
+bcftools query -l $VCF >${VCF%%vcf}sample_list
+
+while read SAMPLE; do
+    echo "$SAMPLE"
+    vcftools --vcf $VCF --indv $SAMPLE --recode --recode-INFO-all --out $SAMPLE
+    java -jar /DATA/home/mjahani/bin/NGSEPcore_4.2.0/NGSEPcore.jar VCFConverter -rrBLUP -i ${SAMPLE}.recode.vcf -o ${VCF%%vcf}_${SAMPLE}
+    cat ${VCF%%vcf}_${SAMPLE}_rrBLUP.in >>${VCF%%.vcf}_rrBLUP.in
+    rm ${VCF%%vcf}_${SAMPLE}_rrBLUP.in ${VCF%%vcf}_${SAMPLE}_rrBLUP_samples.txt
+done <${VCF%%vcf}sample_list
