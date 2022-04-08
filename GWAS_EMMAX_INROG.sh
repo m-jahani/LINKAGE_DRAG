fca1f7e838fb0c050c741296cd0a1ca879f1c0bd
Mojtaba Jahani <moja987@gmail.com>
2022-04-08 16:45:43 -0700

    small edit

--- GWAS_EMMAX_INROG.sh
+++ GWAS_EMMAX_INROG.sh
@@ -6,8 +6,8 @@ COVARIATE=${PATH}/Annuus.ann_fullsam.tranche90_snps_bi_AN50_beagle_AF99.cov
 MAP=${PATH}/SAM_introgression_donor_ANNUUS.map
 OUT_DIR=/DATA/home/mjahani/LINKADE_DRAG/new_method/GWAS/result
 
-for PHENO in /DATA/home/mjahani/LINKADE_DRAG/new_method/GWAS/phenotype/*
-do
-/DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_ANNUUS_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}
-/DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}
-Rscript ${PATH}/manhattan_beta.R ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}.ps ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}.ps $MAP $OUT_DIR
+for PHENO in /DATA/home/mjahani/LINKADE_DRAG/new_method/GWAS/phenotype/*; do
+    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_ANNUUS_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}
+    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}
+    Rscript ${PATH}/manhattan_beta.R ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}.ps ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}.ps $MAP $OUT_DIR
+done
