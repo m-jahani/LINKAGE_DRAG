89ebfa779331eded13bb0495e43209332e058972
Mojtaba Jahani <moja987@gmail.com>
2022-04-08 16:58:01 -0700

    edit in bash variable phenotype

--- GWAS_EMMAX_INROG.sh
+++ GWAS_EMMAX_INROG.sh
@@ -7,7 +7,7 @@ MAP=${PATH}/SAM_introgression_donor_ANNUUS.map
 OUT_DIR=/DATA/home/mjahani/LINKADE_DRAG/new_method/GWAS/result
 
 for PHENO in /DATA/home/mjahani/LINKADE_DRAG/new_method/GWAS/phenotype/*; do
-    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_ANNUUS_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}
-    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}
-    Rscript ${PATH}/manhattan_beta.R ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENOTYPE##*/}.ps ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENOTYPE##*/}.ps $MAP $OUT_DIR
+    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_ANNUUS_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENO##*/}
+    /DATA/home/mjahani/bin/emmax-intel64 -v -d 10 -t ${PATH}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03 -p $PHENO -k $KINSHIP -c $COVARIATE -o ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03_${PHENO##*/}
+    Rscript ${PATH}/manhattan_beta.R ${OUT_DIR}/SAM_introgression_donor_ANNUUS_maf0.03_${PHENO##*/}.ps ${OUT_DIR}/SAM_introgression_donor_2nd_GERMPLASM_maf0.03${PHENOTYPE##*/}.ps $MAP $OUT_DIR
 done
