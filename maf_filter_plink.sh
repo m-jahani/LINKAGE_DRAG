1c372c7e1db94f53e703a8c6f1bd68452278fe09
Mojtaba Jahani <moja987@gmail.com>
2022-04-09 11:14:02 -0700

    rename the out put

--- maf_filter_plink.sh
+++ maf_filter_plink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-plink --tfile SAM_introgression_donor_ANNUUS --keep sample_list_for_phenotype --maf 0.03 --recode transpose --out SAM_introgression_donor_ANNUUS_maf0.03        #505038
-plink --tfile SAM_introgression_donor_2nd_GERMPLASM --keep sample_list_for_phenotype --maf 0.03 --recode transpose --out SAM_introgression_donor_ANNUUS_maf0.03 #5243
+plink --tfile SAM_introgression_donor_ANNUUS --keep sample_list_for_phenotype --maf 0.03 --recode transpose --out SAM_introgression_donor_ANNUUS_maf0.03               #505038
+plink --tfile SAM_introgression_donor_2nd_GERMPLASM --keep sample_list_for_phenotype --maf 0.03 --recode transpose --out SAM_introgression_donor_2nd_GERMPLASM_maf0.03 #5243
