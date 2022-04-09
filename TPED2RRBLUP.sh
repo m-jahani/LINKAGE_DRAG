0c140516e3bb98ac0e3f5cc5a3ad039e11930626
Mojtaba Jahani <moja987@gmail.com>
2022-04-09 11:47:51 -0700

    loop through each sample for RAM efficiency

--- TPED2RRBLUP.sh
+++ TPED2RRBLUP.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 TFILE=$1
-plink --tfile $TFILE --recode vcf --out $TFILE
-java -jar /DATA/home/mjahani/bin/NGSEPcore_4.2.0/NGSEPcore.jar VCFConverter -rrBLUP -i ${TFILE}.vcf -o $TFILE
+plink --tfile $TFILE --recode vcf-iid --out $TFILE
+
+bcftools query -l ${TFILE}.vcf >${TFILE}_sample_list
+grep -v "^#" ${TFILE}.vcf | awk '{print $1":"$2}' >${TFILE}_SNP_list
+while read SAMPLE; do
+    echo "$SAMPLE"
+    vcftools --vcf ${TFILE}.vcf --indv $SAMPLE --recode --recode-INFO-all --out $SAMPLE
+    java -jar /DATA/home/mjahani/bin/NGSEPcore_4.2.0/NGSEPcore.jar VCFConverter -rrBLUP -i ${SAMPLE}.recode.vcf -o ${TFILE}_${SAMPLE}
+    cat ${TFILE}_${SAMPLE}_rrBLUP.in >>${TFILE}_rrBLUP.in
+    rm ${TFILE}_${SAMPLE}_rrBLUP.in ${TFILE}_${SAMPLE}_rrBLUP_samples.txt ${SAMPLE}.log ${SAMPLE}.recode.vcf
+done <${TFILE}_sample_list
