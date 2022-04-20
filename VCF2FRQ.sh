6a3999222ea95370573ddd994068571e877ac35b
Mojtaba Jahani <moja987@gmail.com>
2022-04-19 17:08:32 -0700

    remove header

--- VCF2FRQ.sh
+++ VCF2FRQ.sh
@@ -3,5 +3,5 @@ VCF=$1
 
 vcftools --vcf $VCF --freq --out ${VCF%%.vcf}
 
-awk -F "\t|:" '{print $1":"$2,$8}' ${VCF%%.vcf}.frq >${VCF%%.vcf}.frequency
+awk -F "\t|:" '{print $1":"$2,$8}' ${VCF%%.vcf}.frq | sed 1d >${VCF%%.vcf}.frequency
 rm ${VCF%%.vcf}.frq
