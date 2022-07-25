04ff56d8c4eac05d0eec28fd0d4e4e9668518b8b
Mojtaba Jahani <moja987@gmail.com>
2022-07-24 23:18:22 -0700

    parallel computation feature

--- hybrid_prediction/Cross_simulation.R
+++ hybrid_prediction/Cross_simulation.R
@@ -14,21 +14,25 @@ fread(TPED) -> data
 fread(TFAM) %>% 
   select(V1) -> IDs
 
-registerDoParallel(cores=2)
+data %>%
+  select(1:4) %>%
+  arrange(as.numeric(V1),as.numeric(V4)) -> four_first_column
+  
+
+registerDoParallel(cores=150)
 seq(5,ncol(data),2) -> SAMPLES
 SAMPLES ->  SAMPLES1
 
 for (i in SAMPLES) {
   SAMPLES1[SAMPLES1 != i] -> SAMPLES1
   for (j in SAMPLES1) {
-    result <- select(data,1:4)
-    for (r in 1:100) {
+    result <- foreach(r=1:100, .combine='cbind', .errorhandling='stop') %dopar% {
       data %>%
         select(1:4,
                hap1 =i,
-               hap2 = i+1) %>%
-        mutate(randomizer = sample(2, size = nrow(data), replace = TRUE)) %>%
-        mutate(gamet1 = ifelse(randomizer == 1,hap1,hap2)) %>%
+               hap2 = i+1) %>% 
+        mutate(randomizer = sample(2, size = nrow(data), replace = TRUE)) %>% 
+        mutate(gamet1 = ifelse(randomizer == 1,hap1,hap2)) %>% 
         select(1:4,
                gamet1) %>%
         full_join(.,
@@ -39,20 +43,22 @@ for (i in SAMPLES) {
                                        randomizer = sample(2, size = nrow(data), replace = TRUE)),
                                 gamet2 = ifelse(randomizer == 1,hap1,hap2)),
                          1:4,
-                         gamet2)) %>%
-        select(1:4,
-               !!paste0(IDs[which(SAMPLES==i),1],
+                         gamet2)) %>% 
+        arrange(as.numeric(V1),as.numeric(V4)) %>%
+        select(!!paste0(IDs[which(SAMPLES==i),1],
                         "_",
                         IDs[which(SAMPLES==j),1],
                         "_hap1_r",r)  := gamet1,
                !!paste0(IDs[which(SAMPLES==i),1],
                         "_",
                         IDs[which(SAMPLES==j),1],
-                        "_hap2_r",r) := gamet2) %>%
-        full_join(.,result) -> result
+                        "_hap2_r",r) := gamet2) 
     }
     
-    fwrite(result,
+cbind(four_first_column,result) -> full_result
+rm(result)
+    
+    fwrite(full_result,
            paste0(SAVE_DIR,
                   "/",
                   IDs[which(SAMPLES==i),1],
@@ -62,8 +68,7 @@ for (i in SAMPLES) {
            sep = "\t",
            col.names = F)
     
-    result %>%
-      select(1:4) %>%
+    four_first_column %>%
       fwrite(paste0(SAVE_DIR,
                     "/",
                     IDs[which(SAMPLES==i),1],
@@ -78,12 +83,12 @@ for (i in SAMPLES) {
                       "_",
                       IDs[which(SAMPLES==j),1],
                       "_R_",
-                      row_number())) %>%
+                      row_number())) %>%  
       mutate(X2 = X1,
              X3 = 0,
              X4 = 0,
              X5 = 0,
-             X6 = -9) %>%
+             X6 = -9) %>% 
       fwrite(paste0(SAVE_DIR,
                     "/",
                     IDs[which(SAMPLES==i),1],
@@ -95,5 +100,4 @@ for (i in SAMPLES) {
   }
   
 }
-
 
